a368a99d178f0df102ad748ecb64990b1d04876b
Hua Jin <huazjin16@gmail.com>
2024-03-08 00:38:46 -0600

    created forms for the login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="enter email..."
+          value={data.email}
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="enter password..."
+          value={data.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Register() {
-  return <div>Register</div>;
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const registerUser = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="enter name..."
+          value={data.name}
+          onChange={handleChange}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="enter email..."
+          value={data.email}
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="enter password..."
+          value={data.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
