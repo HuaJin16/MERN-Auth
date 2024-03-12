5088ad82d8f8ae41ef9a3b4fd557ff540776197d
Hua Jin <huazjin16@gmail.com>
2024-03-12 16:42:04 -0500

    implemented a JSONWebToken to authenticate the user throughout the application

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { hashPassword, comparePassword } = require('../helpers/auth');
+const jwt = require('jsonwebtoken');
 
 const test = (req, res) => {
     res.json('test is working');
@@ -60,7 +61,10 @@ const loginUser = async(req, res) => {
         // user.password is the password provided when the user registered (in the database)
         const match = await comparePassword(password, user.password);
         if(match) {
-            res.json('passwords match')
+            jwt.sign({email: user.email, id: user._id, name: user.name}, process.env.JWT_SECRET, {}, (err, token) => {
+                if(err) throw err;
+                res.cookie('token', token).json(user);
+            })
         }
         if(!match) {
             res.json({
--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const app = express();
 
 // database connection
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Database connected'))
 .catch((err) => console.log("Database not connected", err))
 
-const app = express();
-
 // middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/', require('./routes/authRoutes'));
 
